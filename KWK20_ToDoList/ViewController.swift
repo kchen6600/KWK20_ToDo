1ca73322467d8e20f838ff2a0db61494955d50bd
Karen Chen <kchen6600@gmail.com>
2020-05-29 15:25:49 -0400

    Initial commit

---
+++ KWK20_ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KWK20_ToDoList
+//
+//  Created by Karen Chen on 5/29/20.
+//  Copyright © 2020 Karen Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KWK20_ToDoListTests/KWK20_ToDoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  KWK20_ToDoListTests.swift
+//  KWK20_ToDoListTests
+//
+//  Created by Karen Chen on 5/29/20.
+//  Copyright © 2020 Karen Chen. All rights reserved.
+//
+
+import XCTest
+@testable import KWK20_ToDoList
+
+class KWK20_ToDoListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
