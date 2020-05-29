de5fd063e6e624ee0cb0563703b61bf81866ff36
Karen Chen <kchen6600@gmail.com>
2020-05-29 16:39:10 -0400

    iteration 0

---
+++ KWK20_ToDoList/TableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  TableViewController.swift
+//  KWK20_ToDoList
+//
+//  Created by Karen Chen on 5/29/20.
+//  Copyright © 2020 Karen Chen. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    
+    var toDos : [ToDo] = []
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        
+        return [swift, dog]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "!" + toDo.name
+        }
+        else{
+            cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+    }
+
+
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+}
---
+++ KWK20_ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  KWK20_ToDoList
+//
+//  Created by Karen Chen on 5/29/20.
+//  Copyright © 2020 Karen Chen. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
